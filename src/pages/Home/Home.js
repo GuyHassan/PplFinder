aa319aa899b0c1450d4e15cd037d0698b63e9074
Guy Hassan <33221427+GuyHassan@users.noreply.github.com>
2021-10-10 16:54:53 +0300

    Modify Home components, add some logics

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Text from "components/Text";
 import UserList from "components/UserList";
 import { usePeopleFetch } from "hooks";
 import * as S from "./style";
 
 const Home = () => {
-  const { users, isLoading } = usePeopleFetch();
+  const { users, isLoading, fetchUsers } = usePeopleFetch();
+  const [usersList, setUsersList] = useState([]);
+  const [countries, setCountries] = useState(new Set())
+
+  useEffect(() => {
+    setUsersList(users)
+  }, [users])
+
+  useEffect(() => {
+    setCountries(prev => {
+      users.forEach(user => {
+        prev.add(user.location.country);
+      })
+      return new Set(Array.from(prev).slice(0, 7));
+    })
+  }, [users])
 
   return (
     <S.Home>
       <S.Content>
         <S.Header>
-          <Text size="64px" bold>
+          <Text size="50px" bold>
             People Finder
           </Text>
         </S.Header>
-        <UserList users={users} isLoading={isLoading} />
+        <UserList
+          users={usersList}
+          isLoading={isLoading}
+          countries={Array.from(countries)}
+          loadMorePpl={fetchUsers} />
       </S.Content>
+
     </S.Home>
   );
 };
