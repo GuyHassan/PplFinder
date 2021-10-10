be96c14acaa7e76cc521554a38c741067e7eb6f5
Guy Hassan <33221427+GuyHassan@users.noreply.github.com>
2021-10-10 16:39:23 +0300

    modify appRouter, add new route favorite

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "pages";
+import { Favorite } from "pages";
 import { ThemeProvider } from "theme";
 import NavBar from "components/NavBar";
 
 const AppRouter = () => {
+
   return (
     <ThemeProvider>
       <Router>
         <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/favorite" component={Favorite} />
         </Switch>
       </Router>
     </ThemeProvider>
+
   );
 };
 
